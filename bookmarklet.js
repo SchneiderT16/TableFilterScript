635cec160e511762cef8c1cf624cd7def4cc613c
SchneiderT16 <theo.schneider12@gmail.com>
2018-06-06 11:23:10 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function () { 
     var s = document.createElement('script');
     s.setAttribute("type", "text/javascript");
-    s.setAttribute("src", "https://rawgit.com/Sjord/tablefilter/master/tablefilter.js");
+    s.setAttribute("src", "https://raw.githubusercontent.com/SchneiderT16/TableFilterScript/master/tablefilter.js");
     document.body.appendChild(s);
 })();
